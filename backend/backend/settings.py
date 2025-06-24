e90dff433e952050e5fb1da996ae838dee2629fd
ShehanDissanayaka <chamodshehan2019@gmail.com>
2025-06-24 22:43:57 +0530

    Add Procfile and adjust settings for Railway deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 # Static Files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media
 MEDIA_URL = '/media/'
